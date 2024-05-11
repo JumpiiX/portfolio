617a31d636a6c7a0edd8f90ca588dda862d6a666
CodingNerd187 <unterguggenberger.david@gmail.com>
2024-05-11 19:10:07 +0200

    tutorial part 5

--- src/javascript/World/Sections/InformationSection.js
+++ src/javascript/World/Sections/InformationSection.js
@@ -18,54 +18,16 @@ export default class InformationSection
         this.container = new THREE.Object3D()
         this.container.matrixAutoUpdate = false
 
-        this.setStatic()
-        this.setBaguettes()
+
+
         this.setLinks()
         this.setActivities()
         this.setTiles()
     }
 
-    setStatic()
-    {
-        this.objects.add({
-            base: this.resources.items.informationStaticBase.scene,
-            collision: this.resources.items.informationStaticCollision.scene,
-            floorShadowTexture: this.resources.items.informationStaticFloorShadowTexture,
-            offset: new THREE.Vector3(this.x, this.y, 0),
-            mass: 0
-        })
-    }
 
-    setBaguettes()
-    {
-        this.baguettes = {}
 
-        this.baguettes.x = - 4
-        this.baguettes.y = 6
 
-        this.baguettes.a = this.objects.add({
-            base: this.resources.items.informationBaguetteBase.scene,
-            collision: this.resources.items.informationBaguetteCollision.scene,
-            offset: new THREE.Vector3(this.x + this.baguettes.x - 0.56, this.y + this.baguettes.y - 0.666, 0.2),
-            rotation: new THREE.Euler(0, 0, - Math.PI * 37 / 180),
-            duplicated: true,
-            shadow: { sizeX: 0.6, sizeY: 3.5, offsetZ: - 0.15, alpha: 0.35 },
-            mass: 1.5,
-            // soundName: 'woodHit'
-        })
-
-        this.baguettes.b = this.objects.add({
-            base: this.resources.items.informationBaguetteBase.scene,
-            collision: this.resources.items.informationBaguetteCollision.scene,
-            offset: new THREE.Vector3(this.x + this.baguettes.x - 0.8, this.y + this.baguettes.y - 2, 0.5),
-            rotation: new THREE.Euler(0, - 0.5, Math.PI * 60 / 180),
-            duplicated: true,
-            shadow: { sizeX: 0.6, sizeY: 3.5, offsetZ: - 0.15, alpha: 0.35 },
-            mass: 1.5,
-            sleep: false,
-            // soundName: 'woodHit'
-        })
-    }
 
     setLinks()
     {
