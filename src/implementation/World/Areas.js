aca5a358e6aba6cc33376079e8c9c04d9e79fd01
CodingNerd187 <unterguggenberger.david@gmail.com>
2024-05-12 18:49:14 +0200

    setmouse

--- src/implementation/World/Areas.js
+++ src/implementation/World/Areas.js
@@ -25,7 +25,6 @@ export default class Areas
 
     setMouse()
     {
-
         this.mouse = {}
         this.mouse.raycaster = new THREE.Raycaster()
         this.mouse.coordinates = new THREE.Vector2()
