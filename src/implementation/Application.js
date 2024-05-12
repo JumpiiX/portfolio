07167376a5d4842a9df6399287686d245df96f11
CodingNerd187 <unterguggenberger.david@gmail.com>
2024-05-12 20:14:48 +0200

    changed document title emoji

--- src/implementation/Application.js
+++ src/implementation/Application.js
@@ -248,7 +248,7 @@ export default class Application
         {
             this.title.position = Math.round(this.title.absolutePosition % this.title.width)
 
-            document.title = `${'_'.repeat(this.title.width - this.title.position)}🚗${'_'.repeat(this.title.position)}`
+            document.title = `${'_'.repeat(this.title.width - this.title.position)}🚐${'_'.repeat(this.title.position)}`
         }, this.title.frequency)
     }
 
