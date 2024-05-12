b7d89007a8e33c10a6aea95044c308f48ea56520
CodingNerd187 <unterguggenberger.david@gmail.com>
2024-05-12 22:13:50 +0200

    restructure for github pages

--- vite.config.js
+++ vite.config.js
@@ -2,19 +2,22 @@ import restart from 'vite-plugin-restart'
 import glsl from 'vite-plugin-glsl'
 
 export default {
-    root: '.',
-    publicDir: 'static/',
-    server: {
-        host: true,
-        open: !('SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env)
-    },
-    build: {
-        outDir: 'dist',
-        emptyOutDir: true,
-        sourcemap: true
-    },
-    plugins: [
-        glsl(),
-        restart({ restart: ['static/**', ] })
-    ],
+    root: 'src/',
+    publicDir: '../static/',
+    server:
+        {
+            host: true,
+            open: !('SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env)
+        },
+    build:
+        {
+            outDir: '../dist',
+            emptyOutDir: true,
+            sourcemap: true
+        },
+    plugins:
+        [
+            glsl(),
+            restart({ restart: [ '../static/**', ] })
+        ],
 }
